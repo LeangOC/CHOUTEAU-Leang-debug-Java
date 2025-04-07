53ba8f74e35bca206d17e844b997b503df1fa3d2
Leon MiniPcIntel <leon@mail>
2025-04-07 10:23:58 +0200

    main implementatiton

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,31 +4,30 @@ import java.util.*;
 public class AnalyticsCounter {
 	private ISymptomReader reader;
 	private ISymptomWriter writer;
-    public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer){
+
+	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
 		this.reader = reader;
 		this.writer = writer;
-    }
+	}
 
-	public List<String> getSymptoms(){
+	public List<String> getSymptoms() {
 		return reader.GetSymptoms();
 	}
-	public Map<String,Integer> countSymptoms(List<String> symptoms){
-		Map<String,Integer> nbrSymptoms = new HashMap<>();
+
+	public Map<String, Integer> countSymptoms(List<String> symptoms) {
+		Map<String, Integer> nbrSymptoms = new HashMap<>();
 		for (String monSymptoms : symptoms) {
-			nbrSymptoms.put(monSymptoms,nbrSymptoms.getOrDefault(monSymptoms,0) + 1);
+			nbrSymptoms.put(monSymptoms, nbrSymptoms.getOrDefault(monSymptoms, 0) + 1);
 		}
 		return nbrSymptoms;
 	}
 
-	public Map<String,Integer> sortSymptoms(Map<String, Integer> symptoms){
+	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) {
 		Map<String, Integer> mySort = new TreeMap<>(symptoms);
 		return mySort;
 	}
-	public void writeSymptoms(Map<String,Integer> symptoms){
-	writer.writeSymptoms(symptoms);
-	}
 
-	public static void main(String args[]) throws Exception {
-
+	public void writeSymptoms(Map<String, Integer> symptoms) {
+		writer.writeSymptoms(symptoms);
 	}
 }
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -0,0 +1,15 @@
+package com.hemebiotech.analytics;
+import java.util.List;
+import java.util.Map;
+public class Main {
+    public static void main(String args[]) throws Exception {
+        ISymptomReader reader = new ReadSymptomDataFromFile("symptoms.txt");
+        ISymptomWriter writer = new WriteSymptomDataToFile("result.out");
+        AnalyticsCounter myAnalytics = new AnalyticsCounter( reader, writer);
+        List<String> symptoms = myAnalytics.getSymptoms();
+        Map<String,Integer> myCount = myAnalytics.countSymptoms(symptoms);
+        System.out.println(myCount);
+        Map<String,Integer> mySort = myAnalytics.sortSymptoms(myCount);
+        myAnalytics.writeSymptoms(mySort);
+    }
+}
